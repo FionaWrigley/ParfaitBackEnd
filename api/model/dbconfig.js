59a058b1d9ac3ad45fe7b059906f5c0e36ab9826
unhorsedFi <fionaholt@hotmail.com>
2021-04-25 17:01:14 +1000

    update start scripts

--- api/model/dbconfig.js
+++ api/model/dbconfig.js
@@ -5,6 +5,7 @@ module.exports = {
     connectionLimit : 10,
     host: process.env.DB_HOST, 
     user: process.env.DB_USER , 
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT , 
     database: process.env.DB_DATABASE,
     dateStrings: true
