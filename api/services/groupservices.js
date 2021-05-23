1255f7da0fd9e82fd1928a73a696ddf19be42b47
unhorsedFi <fionaholt@hotmail.com>
2021-05-23 23:12:26 +1000

    min-date group schedule bug fix

--- api/services/groupservices.js
+++ api/services/groupservices.js
@@ -47,93 +47,91 @@ const imageDataURI = require('image-data-uri')
 // group->group members->day->event
 //Generate a new record for each day for events that span multiple days
 ///////////////////////////////////////////////////////////////////////////////
-getGroupSchedules: function (gID, minDateStr, numberOfDays, userID, cb){
+// getGroupSchedules: function (gID, minDateStr, numberOfDays, userID, cb){
 
-    let minDate = new Date(minDateStr);
+//     let minDate = new Date(minDateStr);
 
-    if (numberOfDays > 365){ numberOfDays = 365} //schedule should never return more than a year
+//     if (numberOfDays > 365){ numberOfDays = 365} //schedule should never return more than a year
 
-    let maxDate = datefns.add(minDate, {days: numberOfDays});
-    let maxDateStr = datefns.format(maxDate, 'yyyy-MM-dd');
+//     let maxDate = datefns.add(minDate, {days: numberOfDays});
+//     let maxDateStr = maxDate.toISOString().slice(0,10);
 
-    group.getGroupSchedule(gID, minDateStr, maxDateStr, userID, (result) => {
+//     group.getGroupSchedule(gID, minDateStr, maxDateStr, userID, (result) => {
         
-        let groupSched = {};
+//         let groupSched = {};
        
-        if(result.length > 0){ //some results were returned from db so create a new group object
+//         if(result.length > 0){ //some results were returned from db so create a new group object
 
-           groupSched = {groupID: gID, groupName: result[0].groupName, groupDescripton: result[0].groupDescription, groupPic: result[0].groupPic, members: []}
-       }
+//            groupSched = {groupID: gID, groupName: result[0].groupName, groupDescripton: result[0].groupDescription, groupPic: result[0].groupPic, members: []}
+//        }
 
-        for(i=0; i < result.length; i++){ //foreach db record, insert into group schedule object
+//         for(i=0; i < result.length; i++){ //foreach db record, insert into group schedule object
 
-            //check if the member already exists as a group member
-            let memberIndex = groupSched.members.findIndex(ind => ind.memberID == (result[i].memberID));
+//             //check if the member already exists as a group member
+//             let memberIndex = groupSched.members.findIndex(ind => ind.memberID == (result[i].memberID));
           
-            //if member hasn't already been added to the group, add member
-            if(memberIndex < 0){
-                let newGroupMember = {memberID: result[i].memberID, fname: result[i].fname, lname: result[i].lname, profilePic: result[i].profilePic, adminFlag: result[i].adminFlag, acceptedFlag: result[i].acceptedFlag, events: []}
-                groupSched.members.push(newGroupMember);
+//             //if member hasn't already been added to the group, add member
+//             if(memberIndex < 0){
+//                 let newGroupMember = {memberID: result[i].memberID, fname: result[i].fname, lname: result[i].lname, profilePic: result[i].profilePic, adminFlag: result[i].adminFlag, acceptedFlag: result[i].acceptedFlag, events: []}
+//                 groupSched.members.push(newGroupMember);
                 
-                memberIndex = groupSched.members.length-1; //index of the new member record
+//                 memberIndex = groupSched.members.length-1; //index of the new member record
 
-                for(days = 0; days < numberOfDays; days++){
-                    groupSched.members[memberIndex].events.push([]);
-                }
-            }
+//                 for(days = 0; days < numberOfDays; days++){
+//                     groupSched.members[memberIndex].events.push([]);
+//                 }
+//             }
 
-            let startDate = new Date(result[i].startDate);
-            let endDate = new Date(result[i].endDate);
-            //event runs over multiple days but starts before selected date.
-            //Ignore anything before 00:00 on selected date
-            if (datefns.isBefore(startDate, minDate)){
-                    result[i].startDate = minDate.toISOString().slice(0,10);
-                    result[i].startTime = '00:00:00';
-            }
+//             let startDate = new Date(result[i].startDate);
+//             let endDate = new Date(result[i].endDate);
+//             //event runs over multiple days but starts before selected date.
+//             //Ignore anything before 00:00 on selected date
+//             if (datefns.isBefore(startDate, minDate)){
+//                     result[i].startDate = minDate.toISOString().slice(0,10);
+//                     startDate = minDate.toISOString().slice(0,10);
+//                     result[i].startTime = '00:00:00';
+//             }
 
-            //event runs over multiple days and finishes after the 30 day window.
-            //Ignore anything after 11:59 30 days after current date
-            if (datefns.isAfter(endDate, maxDate)){
-                result[i].endDate = maxDate.toISOString().slice(0,10);
-                result[i].endTime = '23:59:00';
-            }
+//             //event runs over multiple days and finishes after the 30 day window.
+//             //Ignore anything after 11:59 30 days after current date
+//             if (datefns.isAfter(endDate, maxDate)){
+//                 result[i].endDate = maxDate.toISOString().slice(0,10);
+//                 endDate = maxDate.toISOString().slice(0,10);
+//                 result[i].endTime = '23:59:00';
+//             }
 
-            let d = startDate;
+//             let d = startDate;
 
-            while(datefns.isBefore(d, endDate) || datefns.isEqual(d, endDate)){
+//             while(datefns.isBefore(d, results[i].endDate) || datefns.isEqual(d, results[i].endDate)){
                 
-                let dateIndex = datefns.differenceInDays(d, minDate);
-                if(datefns.isEqual(d, startDate) && datefns.isEqual(d, endDate)){
+//                 let dateIndex = datefns.differenceInDays(d, minDate);
+//                 if(datefns.isEqual(d, results[i].startDate) && datefns.isEqual(d, results[i].endDate)){
 
-                    let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: result[i].startDate, startTime: result[i].startTime, endTime: result[i].endTime}
-                    groupSched.members[memberIndex].events[dateIndex].push(newEvent);
-                    //groupSched.members[memberIndex].events.push(newEvent);
+//                     let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: result[i].startDate, startTime: result[i].startTime, endTime: result[i].endTime}
+//                     groupSched.members[memberIndex].events[dateIndex].push(newEvent);
 
-                }else if(datefns.isEqual(d, startDate) && datefns.isBefore(d, endDate)){
+//                 }else if(datefns.isEqual(d, results[i].startDate) && datefns.isBefore(d, results[i].endDate)){
 
-                    let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: result[i].startDate, startTime: result[i].startTime, endTime: '11:59:00'}
-                    groupSched.members[memberIndex].events[dateIndex].push(newEvent);
-                    //groupSched.members[memberIndex].events.push(newEvent);
+//                     let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: result[i].startDate, startTime: result[i].startTime, endTime: '11:59:00'}
+//                     groupSched.members[memberIndex].events[dateIndex].push(newEvent);
 
-                }else if(datefns.isAfter(d, startDate) && datefns.isBefore(d, endDate)){
+//                 }else if(datefns.isAfter(d, results[i].startDate) && datefns.isBefore(d, results[i].endDate)){
 
-                    let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: d.toISOString().slice(0,10), startTime: '00:00:00', endTime: '11:59:00'}
-                    groupSched.members[memberIndex].events[dateIndex].push(newEvent);
-                    //groupSched.members[memberIndex].events.push(newEvent);
+//                     let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: d.toISOString().slice(0,10), startTime: '00:00:00', endTime: '11:59:00'}
+//                     groupSched.members[memberIndex].events[dateIndex].push(newEvent);
 
-                }else if(datefns.isAfter(d, startDate) && datefns.isEqual(d, endDate)){
+//                 }else if(datefns.isAfter(d, results[i].startDate) && datefns.isEqual(d, results[i].endDate)){
 
-                    let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: result[i].endDate, startTime: '00:00:00', endTime: result[i].endTime}
-                    groupSched.members[memberIndex].events[dateIndex].push(newEvent);
-                    //groupSched.members[memberIndex].events.push(newEvent);
-                }
-                //increment date
-                d = datefns.add(d, {days: 1});
-            }
-        }
-    cb(groupSched);
-    })
-   },
+//                     let newEvent = {eventID: result[i].eventID, eventName: result[i].eventName, eventDescription: result[i].eventDescription, eventDate: result[i].endDate, startTime: '00:00:00', endTime: result[i].endTime}
+//                     groupSched.members[memberIndex].events[dateIndex].push(newEvent);
+//                 }
+//                 //increment date
+//                 d = datefns.add(d, {days: 1});
+//             }
+//         }
+//     cb(groupSched);
+//     })
+//    },
 
    ////////////////////////////////////////////////////////////////////////////////
 //Change the format of the data from a flat file to an object with nested arrays 
@@ -152,6 +150,8 @@ getGroupSchedules2: function (gID, minDateStr, numberOfDays, userID, cb){
     
     let maxDate = datefns.add(minDate, {days: adjustedDays});
     let maxDateStr = datefns.format(maxDate, 'yyyy-MM-dd');
+    //maxDate.toISOString().slice(0,10);
+
 
     group.getGroupDetails(gID, userID, (result) => {
 
@@ -171,10 +171,10 @@ getGroupSchedules2: function (gID, minDateStr, numberOfDays, userID, cb){
                         
                         let mIndex = groupSched.members.length-1;
 
-                        let numDays1 = numberOfDays;
-                        //numDays1++;
+                        //let numDays1 = numberOfDays;
+                       
 
-                        for(days = 0; days < numDays1; days++){
+                        for(days = 0; days < numberOfDays; days++){
                                      groupSched.members[mIndex].events.push([]);
                          }
                     }
@@ -185,50 +185,49 @@ getGroupSchedules2: function (gID, minDateStr, numberOfDays, userID, cb){
                                 
                                 //check if the member already exists as a group member
                                 let memberIndex = groupSched.members.findIndex(ind => ind.memberID == (results[i].memberID));
-                                let startDate = new Date(results[i].startDate);
-                                let endDate = new Date(results[i].endDate);
+                                //let startDate = new Date(results[i].startDate);
+                               // let endDate = new Date(results[i].endDate);
                                 //event runs over multiple days but starts before selected date.
                                 //Ignore anything before 00:00 on selected date
-                                if (datefns.isBefore(startDate, minDate)){
-                                    results[i].startDate = minDate.toISOString().slice(0,10);
+                                if (datefns.isBefore(new Date(results[i].startDate), minDate)){
+                                    //results[i].startDate = minDate.toISOString().slice(0,10);
+                                    
+                                    results[i].startDate=datefns.format(minDate, 'yyyy-MM-dd')
                                     results[i].startTime = '00:00:00';
                                 }
 
                                 //event runs over multiple days and finishes after the 30 day window.
                                 //Ignore anything after 11:59 30 days after current date
-                                if (datefns.isAfter(endDate, maxDate)){
-                                    results[i].endDate = maxDate.toISOString().slice(0,10);
+                                if (datefns.isAfter(new Date (results[i].endDate), maxDate)){
+                                    //results[i].endDate = maxDate.toISOString().slice(0,10);
+                                    results[i].endDate = datefns.format(maxDate, 'yyyy-MM-dd');
                                     results[i].endTime = '23:59:00';
                                 }
 
-                                let d = startDate;
+                                let d = new Date (results[i].startDate);
 
-                                while(datefns.isBefore(d, endDate) || datefns.isEqual(d, endDate)){
+                                while(datefns.isBefore(d, new Date(results[i].endDate)) || datefns.isEqual(d, new Date(results[i].endDate))){
                 
-                                    console.log('minDate ', minDate.toDateString())
-                                    console.log('d ', d.toDateString())
-                                    
+ 
                                     let dateIndex = datefns.differenceInDays(d, minDate);
 
-                                    console.log('date diff ', dateIndex)
-
-                                    if(datefns.isEqual(d, startDate) && datefns.isEqual(d, endDate)){
+                                    if(datefns.isEqual(d, new Date(results[i].startDate)) && datefns.isEqual(d, new Date(results[i].endDate))){
 
                                     let newEvent = {eventID: results[i].eventID, eventName: results[i].eventName, eventDescription: results[i].eventDescription, eventDate: results[i].startDate, startTime: results[i].startTime, endTime: results[i].endTime}
                                     
                                     groupSched.members[memberIndex].events[dateIndex].push(newEvent);
                              
-                                    }else if(datefns.isEqual(d, startDate) && datefns.isBefore(d, endDate)){
+                                    }else if(datefns.isEqual(d, new Date(results[i].startDate)) && datefns.isBefore(d, new Date(results[i].endDate))){
 
                                         let newEvent = {eventID: results[i].eventID, eventName: results[i].eventName, eventDescription: results[i].eventDescription, eventDate: results[i].startDate, startTime: results[i].startTime, endTime: '23:59:00'}
                                         groupSched.members[memberIndex].events[dateIndex].push(newEvent);
 
-                                    }else if(datefns.isAfter(d, startDate) && datefns.isBefore(d, endDate)){
+                                    }else if(datefns.isAfter(d, new Date(results[i].startDate)) && datefns.isBefore(d, new Date(results[i].endDate))){
 
-                                        let newEvent = {eventID: results[i].eventID, eventName: results[i].eventName, eventDescription: results[i].eventDescription, eventDate: d.toISOString().slice(0,10), startTime: '00:00:00', endTime: '23:59:00'}
+                                        let newEvent = {eventID: results[i].eventID, eventName: results[i].eventName, eventDescription: results[i].eventDescription, eventDate: datefns.format(d, 'yyyy-MM-dd'), startTime: '00:00:00', endTime: '23:59:00'}
                                         groupSched.members[memberIndex].events[dateIndex].push(newEvent);
 
-                                    }else if(datefns.isAfter(d, startDate) && datefns.isEqual(d, endDate)){
+                                    }else if(datefns.isAfter(d, new Date(results[i].startDate)) && datefns.isEqual(d, new Date(results[i].endDate))){
 
                                         let newEvent = {eventID: results[i].eventID, eventName: results[i].eventName, eventDescription: results[i].eventDescription, eventDate: results[i].endDate, startTime: '00:00:00', endTime: results[i].endTime}
                                         groupSched.members[memberIndex].events[dateIndex].push(newEvent);
